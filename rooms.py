18ea7560bb5a69372f5e3892e1b9aeb93b21a561
Dylan Reed <dylan@dylanreed.comgit config --global github.user awesomeguygit config --global user.email dylan@dylanreed.co>
2012-05-29 08:59:19 -0600

    working on getting some sort of room map

--- rooms.py
+++ rooms.py
@@ -1,5 +1,4 @@
-room1 = {'exits': 'NSEW', 'description':'This is room 1', 'items':'gun','monsters':, 'none'}
-room2 = {'exits': 'S' 'description':'This is room 2', 'items':'gun','monsters':, 'none'}
-room3 = {'exits': 'E' 'description':'This is room 3', 'items':'knife','monsters':, 'none'}
-room4 = {'exits': 'N' 'description':'This is room 4', 'items':'none','monsters':, 'none'}
-room5 = {'exits': 'W' 'description':'This is room 5', 'items':'cross','monsters':, 'none'}
+
+
+def room():
+	
