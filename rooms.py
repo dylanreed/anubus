098ac10449ad197ea9aa57adef42ae421d3f712b
Dylan Reed <dylan@dylanreed.comgit config --global github.user awesomeguygit config --global user.email dylan@dylanreed.co>
2012-05-30 20:37:29 -0600

    trying to figure out how to do the rooms with a .yaml file

--- rooms.py
+++ rooms.py
@@ -1,4 +1,12 @@
+import yaml
 
 
-def room():
-	
+room = open('rooms.yaml')
+roomMap = yaml.load(room)
+room.close()
+print roomMap
+print"----"
+print roomMap['room1']
+print "---"
+print roomMap['room2']
+
